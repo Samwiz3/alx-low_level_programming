191cc8470f53080bef994e031b75ac2e86920ff9
Samuel Marfo <smarfo.antwi@gmail.com>
2023-08-22 13:17:07 +0000

    argc and argv

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main -the display name ofthe program
+ * @argc: int
+ * @argv: string
+ * Return: int
+ */
+
+int main(int argc, char *argv[])
+{
+	if (argc == 1)
+	{
+		printf("%s\n", argv[0]);
+	}
+	return (0);
+}
